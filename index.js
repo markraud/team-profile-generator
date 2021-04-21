56ce9aa95c31d6e472c1a291c04354a6719c11d5
Mark Raudenbush <markraud@hotmail.com>
2021-04-20 22:01:12 -0600

    set up some of the folder structure based in the hw readme

--- index.js
+++ index.js
@@ -1,71 +1,89 @@
+// main function of this file is to do the collection of info using inquirer
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+siteHtml = `
+<!DOCTYPE html>
+<html>
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Link to style sheet if needed -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <!--Let browser know website is optimized for mobile-->
+    <title>Document</title>
+</head>
+
+<body class="container grey darken-3>
+    <div class="row">
+    <div class="col s10 m4 l3">
+      <div class="card-panel small blue white-text">
+        <span class="card-title">Manager</span>
+        <ul class="collection">
+          <li class="collection-item black-text">Name: ${mgrName}</li>
+            <li class="collection-item black-text">Manager</li>
+            <li class="collection-item black-text">id: ${mgrId}</li>
+            <li class="collection-item black-text">email: ${mgrEmail}</li>
+            <li class="collection-item black-text">office num: ${mgrOfficeNum}</li>
+        </ul>
+      </div>
+    </div>
+    <div class="col s10 m4 l3">
+      <div class="card-panel small blue white-text">
+        <span class="card-title">name</span>
+        <ul>
+          <li>title</li>
+          <li>id</li>
+          <li>email</li>
+          <li>github</li>
+        </ul>
+      </div>
+    </div> 
+
+    <!-- JavaScript, jquery and materialize js links -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</body></html>`
+
+const 
+
+if (!mgrName) {}
 inquirer
   .prompt([
     {
       type: 'input',
-      message: 'Enter team memeber name.',
-      name: 'name',
+      message: 'Enter team manager\'s name.',
+      name: 'mgrName',
     },
     {
       type: 'input',
-      message: 'Enter team member job title.',
-      name: 'title',
+      message: 'Enter team manager\'s employee ID.',
+      name: 'mgrId',
     },
     {
       type: 'input',
-      message: 'Enter team member ID',
-      name: 'id',
+      message: 'Enter team manager\'s email address.',
+      name: 'mgrEmail',
+    },
+    {
+      type: 'input',
+      message: 'Enter team manager\'s office number.',
+      name: 'mgrOfficeNum',
     },
   ])
   .then((response) =>{
 
-  const{name,title,id} = response  // this is destructuring the response object
+  const{mgrName,mgrId,mgrEmail,mgrOfficeNum} = response  // destructuring the response 
 
-  fs.writeFile("index.html", `
-  <!DOCTYPE html>
-  <html>
   
-  <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!-- Compiled and minified CSS -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-      <!-- Link to style sheet if needed -->
-      <link rel="stylesheet" href="assets/css/style.css" />
-      <!--Let browser know website is optimized for mobile-->
-      <title>Document</title>
-  </head>
-  
-  <body class="container grey darken-3>
-      <div class="row">
-        <div class="col s12 m6 l3">
-          <div class="card blue-grey darken-1">
-            <div class="card-content white-text">
-              <span class="card-title">Your Information</span>
-              <ul class="collection">
-              <li class="collection-item black-text">Name: ${name}</li>
-              <li class="collection-item black-text">Job: ${title}</li>
-              <li class="collection-item black-text">ID: ${id}</li>
-            </ul>
-            </div>
-            <div class="card-action">
-            <img src="monster.webp" alt="monster truck" width="600" height="600">
-            </div>
-          </div>
-        </div>
-      </div>
-  
-      <!-- JavaScript, jquery and materialize js links -->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-      <script type="text/javascript" src="assets/js/script.js"></script>
-  </body>
-  
-  </html>`,  (err) =>
+
+  fs.writeFile("index.html", siteHtml,  (err) =>
   err ? console.error(err) : console.log('Success!'))
   });
---
+++ src/template.js
@@ -0,0 +1,13 @@
+//help us create the team by running some functions that output a string that is HTML
+
+const generateTeam = team => 
+
+const generateManager = manager => {
+  return ` <div> html ${manager.getName()}</div> etc..`
+}
+
+
+
+modeule.exports = team => {
+  return `boiler plate html`
+}
