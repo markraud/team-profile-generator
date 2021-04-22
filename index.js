96850b4aedb474cb693cfda87319ce31a48ea845
Mark Raudenbush <markraud@hotmail.com>
2021-04-21 20:01:14 -0600

    fixed lib files and created init function for index js

--- index.js
+++ index.js
@@ -2,60 +2,16 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-siteHtml = `
-<!DOCTYPE html>
-<html>
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const teamMembers = [];
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <!--Import Google Icon Font-->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <!-- Link to style sheet if needed -->
-    <link rel="stylesheet" href="assets/css/style.css" />
-    <!--Let browser know website is optimized for mobile-->
-    <title>Document</title>
-</head>
-
-<body class="container grey darken-3>
-    <div class="row">
-    <div class="col s10 m4 l3">
-      <div class="card-panel small blue white-text">
-        <span class="card-title">Manager</span>
-        <ul class="collection">
-          <li class="collection-item black-text">Name: ${mgrName}</li>
-            <li class="collection-item black-text">Manager</li>
-            <li class="collection-item black-text">id: ${mgrId}</li>
-            <li class="collection-item black-text">email: ${mgrEmail}</li>
-            <li class="collection-item black-text">office num: ${mgrOfficeNum}</li>
-        </ul>
-      </div>
-    </div>
-    <div class="col s10 m4 l3">
-      <div class="card-panel small blue white-text">
-        <span class="card-title">name</span>
-        <ul>
-          <li>title</li>
-          <li>id</li>
-          <li>email</li>
-          <li>github</li>
-        </ul>
-      </div>
-    </div> 
-
-    <!-- JavaScript, jquery and materialize js links -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    <script type="text/javascript" src="assets/js/script.js"></script>
-</body></html>`
-
-const 
-
-if (!mgrName) {}
-inquirer
+// wrap everything inside of a init function
+function init() {
+  //get  manager information
+  function makeManager() {
+    inquirer
   .prompt([
     {
       type: 'input',
@@ -82,8 +38,28 @@ inquirer
 
   const{mgrName,mgrId,mgrEmail,mgrOfficeNum} = response  // destructuring the response 
 
-  
+  const manager = new Manager(mgrName, mgrId, mgrEmail, mgrOfficeNum);
+  teamMembers.push(manager);
 
-  fs.writeFile("index.html", siteHtml,  (err) =>
-  err ? console.error(err) : console.log('Success!'))
+  // call makeTeam function
+  makeTeam();
+    //use switch statement
+
   });
+  }
+
+  function makeTeam() {
+    console.log("inside the function");
+  }
+  //get engineer information
+
+  //get intern information
+
+  // create html team file
+  // fs.writeFile("index.html", ,  (err) =>
+  // err ? console.error(err) : console.log('Success!'))
+
+  makeManager();
+}
+
+init();
--- src/template.js
+++ src/template.js
@@ -1,10 +1,15 @@
 //help us create the team by running some functions that output a string that is HTML
 
-const generateTeam = team => 
+const generateTeam = team => {
+  
+  const generateManager = manager => {
+    return ` <div> html ${manager.getName()}</div> etc..`
+  }
 
-const generateManager = manager => {
-  return ` <div> html ${manager.getName()}</div> etc..`
+
+  
 }
+
 
 
 
